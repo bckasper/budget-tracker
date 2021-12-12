7914b37b2d945be37144bac6aa453fcc5013974b
Ben Kasper <bckasper@gmail.com>
2021-12-12 10:56:36 -0500

    added a function for using the indexing DB in the database js file.

--- public/database.js
+++ public/database.js
@@ -6,3 +6,43 @@ export function checkForIndexedDb(){
     return true;
 }
 
+export function usedIndexedDb(dbName, storeName, method, object){
+    return new Promise((resolve, reject) => {
+        const request = window.indexedDB.open(dbName, 1);
+        let db,
+        tx,
+        store;
+
+        request.onupgradeneeded = function(event) {
+            const db = request.result;
+            db.createdObjectStore(storeName, {keyPath: "_id"})
+        };
+
+        request.onerror = function(event) {
+            console.log("There was an error");
+        };
+
+        request.onsuccess = function(event) {
+            db = request.result;
+            tx = db.transaction(storeName, "readwrite");
+            store = tx.objectStore(storeName);
+
+            db.onerror = function(event) {
+                console.log("error");
+            };
+            if (method = "put") {
+                store.put(object);
+            } else if (method = "get") {
+                const all = store.getAll();
+                all.onsuccess = function() {
+                    resolve(all.result);
+                };
+            } else if (method = "delete") {
+                store.delete(object._id);
+            }
+            tx.oncomplete = function() {
+                db.close();
+            };
+        };
+    });
+};
