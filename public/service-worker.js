e8deaf42bea35ecbba963738fb4d5a56cc159953
Ben Kasper <bckasper@gmail.com>
2021-12-12 11:06:30 -0500

    added files to cache in the service workers js

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    'styles.css',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+]
+
