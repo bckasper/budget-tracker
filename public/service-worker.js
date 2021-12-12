e52c045d6ee7d9d84a530847f6f968c2724201f3
Ben Kasper <bckasper@gmail.com>
2021-12-12 11:15:45 -0500

    added functions for cleaning up old caches and what to do on install

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,3 +7,34 @@ const FILES_TO_CACHE = [
     'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
 ]
 
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+self.addEventListener('install', (event)=>{
+    event.waitUntil(
+        caches
+            .open(PRECACHE)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
+    )
+});
+
+// Activates cleaning up old caches
+self.addEventListener('activate', (event)=>{
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName))
+            })
+            .then((deleteCaches) => {
+                return Promise.all(
+                    deleteCaches.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete)
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
+    );
+});
